9568d1b5c65cd17ab3a3db96ed0df9e5928aa1fe
Liuxin <1242129679@qq.com>
2025-04-24 22:35:25 +0800

    Fix for read ManufacturingStatus and set fet enable

--- bq40z50_fg.c
+++ bq40z50_fg.c
@@ -41,9 +41,6 @@
 #define FG_FLAGS_DSG            BIT(6)   // Discharging or Relax
 #define FG_FLAGS_RCA            BIT(9)   // Remaining Capacity Alarm
 
-//#define FG_MAC_CMD_MANUFACTURE_STATUS    0x0057  // 获取制造状态
-//#define FG_MAC_CMD_FET_CONTROL           0x0058  // FET控制命令
-
 // --- 寄存器索引枚举（与地址映射对应）---
 enum bq_fg_reg_idx {
 	BQ_FG_REG_MAC,			// ManufacturerAccess (控制命令接口)
@@ -60,8 +57,6 @@ enum bq_fg_reg_idx {
 	BQ_FG_REG_DC,			// 设计容量 (Design Capacity)
 	BQ_FG_REG_SOH,			// 电池健康度百分比 (State of Health)
 	BQ_FG_REG_MBA,			// ManufacturerBlockAccess (数据闪存块操作接口)
-	FG_MAC_CMD_MANUFACTURE_STATUS,
-	FG_MAC_CMD_FET_CONTROL,
 	NUM_REGS,
 };
 
@@ -73,10 +68,14 @@ enum bq_fg_mac_cmd {
 	FG_MAC_CMD_HW_VER       = 0x0003, // 硬件版本号
 	FG_MAC_CMD_IF_SIG       = 0x0004, // 接口签名验证
 	FG_MAC_CMD_CHEM_ID      = 0x0006, // 读取电池化学ID (用于阻抗匹配)
-	FG_MAC_CMD_GAUGING      = 0x0021,  // 进入校准模式（如Qmax更新）
-	FG_MAC_CMD_FET_EN       = 0x0022, // 进入校准模式（如Qmax更新）
+	FG_MAC_CMD_CHG_FET      = 0x001F, // 充电FET控制（开/关）
+	FG_MAC_CMD_DSG_FET      = 0x0020, // 放电FET控制（开/关）
+	FG_MAC_CMD_GAUGING      = 0x0021, // 进入校准模式（如Qmax更新）
+	FG_MAC_CMD_FET_EN       = 0x0022, // FET使能（开/关）
 	FG_MAC_CMD_SEAL         = 0x0030, // SEAL/UNSEAL设备（安全权限控制）
 	FG_MAC_CMD_DEV_RESET    = 0x0041, // 复位设备（需UNSEAL权限）
+	FG_MAC_CMD_MANUFACTURE_STATUS = 0x0057, // 读取制造商状态（FET状态）
+	FG_MAC_CMD_FET_CONTROL	= 0x0058, // FET控制（开/关）
 };
 
 // --- 设备安全密封状态枚举 ---
@@ -111,17 +110,14 @@ static u8 bq40z50_regs[NUM_REGS] = {
 	0x18,  // 设计容量 (Design Capacity)
 	0x4F,  // 电池健康度百分比 (State of Health)
 	0x44,  // ManufacturerBlockAccess (数据闪存块操作接口)
-	0x57,
-	0x58,
 };
 
 struct bq_fg_chip {
 	struct device *dev;
 	struct i2c_client *client;
 
-	bool fet_enabled;
-	bool chg_test;
-	bool dsg_test;
+	u8 recv_buf[40];
+	u8 send_buf[40];
 
 	struct mutex i2c_rw_lock;
 	struct mutex data_lock;
@@ -191,6 +187,8 @@ static int __fg_write_word(struct i2c_client *client, u8 reg, u16 val)
 {
 	s32 ret;
 
+	bq_log("__fg_write_word: reg=0x%02X, val=0x%02X\n", reg, val);
+
 	ret = i2c_smbus_write_word_data(client, reg, val);
 	if (ret < 0) {
 		bq_err("i2c write word fail: can't write 0x%02X to reg 0x%02X\n",
@@ -237,6 +235,7 @@ static int fg_read_word(struct bq_fg_chip *bq, u8 reg, u16 *val)
 	return ret;
 }
 
+#if 0
 static int fg_write_word(struct bq_fg_chip *bq, u8 reg, u16 val)
 {
 	int ret;
@@ -250,6 +249,7 @@ static int fg_write_word(struct bq_fg_chip *bq, u8 reg, u16 val)
 
 	return ret;
 }
+#endif
 
 static int fg_read_block(struct bq_fg_chip *bq, u8 reg, u8 *buf, u8 len)
 {
@@ -279,19 +279,6 @@ static int fg_write_block(struct bq_fg_chip *bq, u8 reg, u8 *data, u8 len)
 	return ret;
 }
 
-static u8 checksum(u8 *data, u8 len)
-{
-	u8 i;
-	u16 sum = 0;
-
-	for (i = 0; i < len; i++)
-		sum += data[i];
-
-	sum &= 0xFF;
-
-	return 0xFF - sum;
-}
-
 #if 1
 static void fg_print_buf(const char *msg, u8 *buf, u8 len)
 {
@@ -314,20 +301,21 @@ static void fg_print_buf(const char *msg, u8 *buf, u8 len)
 
 static int fg_mac_read_block(struct bq_fg_chip *bq, u16 cmd, u8 *buf, u8 len)
 {
-	int ret;
-	u8 t_buf[40];
+	int ret = -1;
+	u8 t_buf[40] = {0};
 	u8 t_len;
 	int i;
 
-	t_buf[0] = (u8)(cmd >> 8);
-	t_buf[1] = (u8)cmd;
+	t_buf[0] = (u8)cmd;
+	t_buf[1] = (u8)(cmd >> 8);
+
 	ret = fg_write_block(bq, bq->regs[BQ_FG_REG_MBA], t_buf, 2);
 	if (ret < 0)
 		return ret;
 
-	msleep(100);
+	msleep(10);
 
-	ret = fg_read_block(bq, bq->regs[BQ_FG_REG_MBA], t_buf, 36);
+	ret = fg_read_block(bq, bq->regs[BQ_FG_REG_MBA], t_buf, len + 2);
 	if (ret < 0)
 		return ret;
 	t_len = ret;
@@ -337,21 +325,21 @@ static int fg_mac_read_block(struct bq_fg_chip *bq, u16 cmd, u8 *buf, u8 len)
 	for (i = 0; i < t_len - 2; i++)
 		buf[i] = t_buf[i+2];
 
-	return 0;
+	return ret;
 }
 
 static int fg_mac_write_block(struct bq_fg_chip *bq, u16 cmd, u8 *data, u8 len)
 {
 	int ret;
-	u8 cksum;
 	u8 t_buf[40];
 	int i;
 
 	if (len > 32)
 		return -1;
 
-	t_buf[0] = (u8)(cmd >> 8);
-	t_buf[1] = (u8)cmd;
+	t_buf[0] = (u8)cmd;
+	t_buf[1] = (u8)(cmd >> 8);
+
 	for (i = 0; i < len; i++)
 		t_buf[i+2] = data[i];
 
@@ -363,32 +351,37 @@ static int fg_mac_write_block(struct bq_fg_chip *bq, u16 cmd, u8 *data, u8 len)
 	return ret;
 }
 
+static int fg_mac_trigger(struct bq_fg_chip *bq, u16 cmd)
+{
+	int ret;
+	u8 t_buf[2];
+
+	t_buf[0] = (u8)cmd;
+	t_buf[1] = (u8)(cmd >> 8);
+
+	ret = fg_write_block(bq, bq->regs[BQ_FG_REG_MBA], t_buf, 2);
+	if (ret < 0)
+		return ret;
+
+	return ret;
+}
+
 static void fg_read_fw_version(struct bq_fg_chip *bq)
 {
 
 	int ret;
-	u8 buf[36];
+	u8 t_buf[36];
 
-	ret = fg_write_word(bq, bq->regs[BQ_FG_REG_MBA], FG_MAC_CMD_FW_VER);
-
-	if (ret < 0) {
-		bq_err("Failed to send firmware version subcommand:%d\n", ret);
-		return;
-	}
-
-	mdelay(2);
-
-	ret = fg_mac_read_block(bq, bq->regs[BQ_FG_REG_MBA], buf, 11);
+	ret = fg_mac_read_block(bq, FG_MAC_CMD_FW_VER, t_buf, 11);
 	if (ret < 0) {
 		bq_err("Failed to read firmware version:%d\n", ret);
 		return;
 	}
 
 	bq_log("FW Ver:%04X, Build:%04X\n",
-			buf[2] << 8 | buf[3], buf[4] << 8 | buf[5]);
-	bq_log("Ztrack Ver:%04X\n", buf[7] << 8 | buf[8]);
+		t_buf[2] << 8 | t_buf[3], t_buf[4] << 8 | t_buf[5]);
+	bq_log("Ztrack Ver:%04X\n", t_buf[7] << 8 | t_buf[8]);
 }
-
 
 static int fg_read_status(struct bq_fg_chip *bq)
 {
@@ -422,7 +415,6 @@ static int fg_read_rsoc(struct bq_fg_chip *bq)
 	}
 
 	return soc;
-
 }
 
 static int fg_read_temperature(struct bq_fg_chip *bq)
@@ -435,7 +427,6 @@ static int fg_read_temperature(struct bq_fg_chip *bq)
 		bq_err("could not read temperature, ret = %d\n", ret);
 		return ret;
 	}
-	bq_log("temperture: %d\n", temp - 2731);
 
 	return temp - 2731;
 }
@@ -528,7 +519,6 @@ static int fg_read_rm(struct bq_fg_chip *bq)
 	}
 
 	return rm;
-
 }
 
 static int fg_read_cyclecount(struct bq_fg_chip *bq)
@@ -602,9 +592,7 @@ static int fg_get_batt_capacity_level(struct bq_fg_chip *bq)
 		return POWER_SUPPLY_CAPACITY_LEVEL_CRITICAL;
 	else
 		return POWER_SUPPLY_CAPACITY_LEVEL_NORMAL;
-
 }
-
 
 static enum power_supply_property fg_props[] = {
 	POWER_SUPPLY_PROP_STATUS,
@@ -724,7 +712,6 @@ static int fg_get_property(struct power_supply *psy, enum power_supply_property 
 	}
 	return 0;
 }
-static void fg_dump_registers(struct bq_fg_chip *bq);
 
 static int fg_set_property(struct power_supply *psy,
 		enum power_supply_property prop,
@@ -798,189 +785,98 @@ static void fg_psy_unregister(struct bq_fg_chip *bq)
 	power_supply_unregister(bq->fg_psy);
 }
 
-static const u8 fg_dump_regs[] = {
-	0x08, 0x09, 0x0A, 0x0D,
-	0x0F, 0x10, 0x16, 0x3C,
-	0x3D, 0x3E, 0x3F, 0x54,
-	0x55, 0x56, 0x57, 0x71,
-	0x72, 0x74, 0x75, 0x76,
-	0x77, 0x78,
-};
-
-static ssize_t fg_attr_show_Ra_table(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct i2c_client *client = to_i2c_client(dev);
-	struct bq_fg_chip *bq = i2c_get_clientdata(client);
-	u8 t_buf[40];
-	u8 temp_buf[40];
+static int fg_read_mac_status(struct bq_fg_chip *bq) {
 	int ret;
-	int i, idx, len;
-
-	ret = fg_mac_read_block(bq, 0x4102, t_buf, 32);
-	if (ret < 0)
-		return 0;
-
-	idx = 0;
-	len = sprintf(temp_buf, "RaTable:\n");
-	memcpy(&buf[idx], temp_buf, len);
-	idx += len;
-	for (i = 0; i < 15; i++) {
-		len = sprintf(temp_buf, "%d ", t_buf[i*2] << 8 | t_buf[i*2 + 1]);
-		memcpy(&buf[idx], temp_buf, len);
-		idx += len;
-	}
-
-	return idx;
-}
-
-static ssize_t fg_attr_show_Qmax(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct i2c_client *client = to_i2c_client(dev);
-	struct bq_fg_chip *bq = i2c_get_clientdata(client);
-	int ret;
-	u8 t_buf[32];
-	u8 temp_buf[32];
-	int i, idx, len;
-
-	memset(t_buf, 0, 64);
-	/* GaugingStatus3 register contains Qmax value */
-	ret = fg_mac_read_block(bq, 0x0075, t_buf, 24);
-	if (ret < 0)
-		return 0;
-	idx = 0;
-	for (i = 0; i < 4; i++) {
-		len = sprintf(temp_buf, "Qmax Cell %d = %d\n", i, (t_buf[i*2] << 8) | t_buf[i*2+1]);
-		memcpy(&buf[idx], temp_buf, len);
-		idx += len;
-	}
-
-	return idx;
-}
-
-// 读取FET状态
-static int fg_read_fet_status(struct bq_fg_chip *bq)
-{
-	int ret;
-	u8 buf[36];
-
-	ret = fg_read_block(bq, bq->regs[FG_MAC_CMD_MANUFACTURE_STATUS], buf, 32);
+	u8 *t_buf = bq->recv_buf;
+	
+	ret = fg_mac_read_block(bq, FG_MAC_CMD_MANUFACTURE_STATUS, t_buf, 4);
 	if (ret < 0) {
-		bq_err("Failed to read FET status:%d ", ret);
+		bq_err("Failed to read FET status:%d", ret);
 		return ret;
 	}
-
-	bq->fet_enabled = !!(buf[0] & BIT(4));
-	bq->chg_test = !!(buf[0] & BIT(1));
-	bq->dsg_test = !!(buf[0] & BIT(2));
-
-//	for(int i=0;i<32;i++)
-//		bq_log("buf[%d]=%d ", buf[i]);
-	//bq_log("FET_EN: %d, CHG_TEST: %d, DSG_TEST: %d\n", 
-	//		bq->fet_enabled, bq->chg_test, bq->dsg_test);
 
 	return 0;
-}
-
-// 设置FET状态
-static int fg_set_fet_status(struct bq_fg_chip *bq)
-{
-	int ret;
-	u8 buf[36] = {0};
-
-	// 首先读取当前状态
-	ret = fg_read_fet_status(bq);
-	if (ret < 0)
-		return ret;
-
-	// 应用逻辑: if [FET_EN]=1 then [FET_EN]=0 elseif [CHG_TEST]=0 and [DSG_TEST]=0 then [CHG_TEST]=1 and [DSG_TEST]=1
-	if (bq->fet_enabled) {
-		bq->fet_enabled = false;
-	} 
-
-	if (!bq->chg_test && !bq->dsg_test) {
-		bq->chg_test = true;
-		bq->dsg_test = true;
-	}
-
-	buf[0] = (u8)(FG_MAC_CMD_FET_CONTROL >> 8);
-	buf[1] = (u8)FG_MAC_CMD_FET_CONTROL ;
-	//buf[0] = (u8)(FG_MAC_CMD_FET_EN >> 8);
-	//buf[1] = (u8)FG_MAC_CMD_FET_EN && 0xFF;
-
-	// 设置相应的位
-	if (bq->fet_enabled)
-		buf[0] |= BIT(4);
-	if (bq->chg_test)
-		buf[0] |= BIT(1);
-	if (bq->dsg_test)
-		buf[0] |= BIT(2);
-	bq_log("bit: %d\n", buf[0]);
-	u16 cmd = FG_MAC_CMD_FET_CONTROL << 8 | 0x06;
-	//ret = fg_write_word(bq, bq->regs[BQ_FG_REG_MBA], cmd);
-	ret = fg_mac_write_block(bq, bq->regs[FG_MAC_CMD_FET_CONTROL], buf, 3);
-	if (ret < 0) {
-		bq_err("Failed to write FET control:%d\n", ret);
-		return ret;
-	}
-
-	mdelay(2);
-
-	ret = fg_mac_read_block(bq, bq->regs[FG_MAC_CMD_FET_CONTROL], buf, 32);
-	if (ret < 0) {
-		bq_err("Failed to read FET control:%d\n", ret);
-		return ret;
-	}
-
-	bq_log("read: %d  %d  %d \n", buf[0], buf[1], buf[2]);
-
-	bq_log("FET control updated: FET_EN: %d, CHG_TEST: %d, DSG_TEST: %d\n", 
-		bq->fet_enabled, bq->chg_test, bq->dsg_test);
-
-	return 0;
-}
-
-// 添加到sysfs接口
-static ssize_t fg_attr_show_fet_status(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	struct i2c_client *client = to_i2c_client(dev);
-	struct bq_fg_chip *bq = i2c_get_clientdata(client);
-	int ret;
-
-	ret = fg_read_fet_status(bq);
-	if (ret < 0)
-		return ret;
-
-	return sprintf(buf, "FET_EN:%d CHG_TEST:%d DSG_TEST:%d\n", 
-			bq->fet_enabled, bq->chg_test, bq->dsg_test);
 }
 
 static ssize_t fg_attr_store_fet_control(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
+	struct device_attribute *attr, const char *buf, size_t count)
 {
 	struct i2c_client *client = to_i2c_client(dev);
 	struct bq_fg_chip *bq = i2c_get_clientdata(client);
+	int ret = -1;
+    int val;
+
+	ret = sscanf(buf, "%d", &val); 
+    if (ret < 0 || ret > 1)
+	{
+		dev_err(dev, "couldn't read val\n");
+        return ret;
+	}
+
+	fg_read_mac_status(bq);
+
+	if(val == 1)
+	{
+		if(bq->recv_buf[0] & BIT(4))
+		{
+			fg_mac_trigger(bq, FG_MAC_CMD_FET_EN);
+			if (ret < 0) {
+				bq_err("Failed to toggle FET control:%d\n", ret);
+				return ret;
+			}
+		}
+		
+		if(!(bq->recv_buf[0] & BIT(1)))
+		{
+			ret = fg_mac_trigger(bq, FG_MAC_CMD_CHG_FET);
+			if (ret < 0) {
+				bq_err("Failed to toggle CHG control:%d\n", ret);
+				return ret;
+			}
+		}
+
+		if(!(bq->recv_buf[0] & BIT(2)))
+		{
+			ret = fg_mac_trigger(bq, FG_MAC_CMD_DSG_FET);
+			if (ret < 0) {
+				bq_err("Failed to toggle DSG control:%d\n", ret);
+				return ret;
+			}
+		}
+	}
+
+	return ret;
+}
+
+static ssize_t fg_attr_show_mac_status(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	struct bq_fg_chip *bq = i2c_get_clientdata(client);
+	u8 *recv_buf = bq->recv_buf;
 	int ret;
 
-	// 触发FET控制逻辑
-	ret = fg_set_fet_status(bq);
+	ret = fg_read_mac_status(bq);
 	if (ret < 0)
 		return ret;
 
-	return count;
+	return sprintf (buf, "\n \
+		[CALTS=%d][LT_TS=%d][ResD=%d][ResC=%d][ResB=%d ][ResA=%d][LED=%d][FUSE=%d]\n \
+		[BBR=%d  ][PF=%d   ][LF=%d  ][FET=%d ][GAUGE=%d][DSG=%d ][CHG=%d][PCHG=%d]\n",
+		!!(recv_buf[1] & BIT(7)), !!(recv_buf[1] & BIT(6)), !!(recv_buf[1] & BIT(5)), !!(recv_buf[1] & BIT(4)),
+		!!(recv_buf[1] & BIT(3)), !!(recv_buf[1] & BIT(2)), !!(recv_buf[1] & BIT(1)), !!(recv_buf[1] & BIT(0)),
+		!!(recv_buf[0] & BIT(7)), !!(recv_buf[0] & BIT(6)), !!(recv_buf[0] & BIT(5)), !!(recv_buf[0] & BIT(4)),
+		!!(recv_buf[0] & BIT(3)), !!(recv_buf[0] & BIT(2)), !!(recv_buf[0] & BIT(1)), !!(recv_buf[0] & BIT(0))
+	);
+
+	return ret;
 }
 
-static DEVICE_ATTR(RaTable, S_IRUGO, fg_attr_show_Ra_table, NULL);
-static DEVICE_ATTR(Qmax, S_IRUGO, fg_attr_show_Qmax, NULL);
-static DEVICE_ATTR(fet_status, S_IRUGO, fg_attr_show_fet_status, NULL);
-static DEVICE_ATTR(fet_control, S_IWUSR, NULL, fg_attr_store_fet_control);
+static DEVICE_ATTR(mac_status, S_IRUGO, fg_attr_show_mac_status, NULL);
+static DEVICE_ATTR(fet_control, 0644, NULL, fg_attr_store_fet_control);
 
 static struct attribute *fg_attributes[] = {
-	&dev_attr_RaTable.attr,
-	&dev_attr_Qmax.attr,
-	&dev_attr_fet_status.attr,
+	&dev_attr_mac_status.attr,
 	&dev_attr_fet_control.attr,
 	NULL,
 };
@@ -989,29 +885,11 @@ static const struct attribute_group fg_attr_group = {
 	.attrs = fg_attributes,
 };
 
-
-static void fg_dump_registers(struct bq_fg_chip *bq)
-{
-#if 0
-	int i;
-	int ret;
-	u16 val;
-
-	for (i = 0; i < ARRAY_SIZE(fg_dump_regs); i++) {
-		msleep(5);
-		ret = fg_read_word(bq, fg_dump_regs[i], &val);
-		if (!ret)
-			bq_log("Reg[%02X] = 0x%04X\n", fg_dump_regs[i], val);
-	}
-#endif
-}
-
 static irqreturn_t fg_btp_irq_thread(int irq, void *dev_id)
 {
 	/*struct bq_fg_chip *bq = dev_id;*/
 
 	/* user can update btp trigger point here*/
-
 
 	return IRQ_HANDLED;
 }
@@ -1028,29 +906,23 @@ static void fg_update_status(struct bq_fg_chip *bq)
 	bq->batt_rm = fg_read_rm(bq);
 
 	mutex_unlock(&bq->data_lock);
-	bq_log("RSOC:%d, Volt:%d, Current:%d, Temperature:%d\n",
-			bq->batt_soc, bq->batt_volt, bq->batt_curr, bq->batt_temp);
 }
 
 static void fg_monitor_workfunc(struct work_struct *work)
 {
 	struct bq_fg_chip *bq = container_of(work, struct bq_fg_chip, monitor_work.work);
 
-	//fg_update_status(bq);
-
-	//fg_dump_registers(bq);
+	fg_update_status(bq);
 
 	schedule_delayed_work(&bq->monitor_work, 5 * HZ);
 }
 
-
 static void determine_initial_status(struct bq_fg_chip *bq)
 {
-	//fg_update_status(bq);
+	fg_update_status(bq);
 
 	fg_btp_irq_thread(bq->client->irq, bq);
 }
-
 
 static struct of_device_id bq_fg_match_table[] = {
 	{.compatible = "ti,bq40z50",},
@@ -1063,7 +935,6 @@ static const struct i2c_device_id bq_fg_id[] = {
 	{},
 };
 MODULE_DEVICE_TABLE(i2c, bq_fg_id);
-
 
 static int bq_fg_probe(struct i2c_client *client)
 {
@@ -1136,7 +1007,7 @@ static int bq_fg_probe(struct i2c_client *client)
 	determine_initial_status(bq);
 
 	INIT_DELAYED_WORK(&bq->monitor_work, fg_monitor_workfunc);
-	schedule_delayed_work(&bq->monitor_work, 5 * HZ);
+	// schedule_delayed_work(&bq->monitor_work, 5 * HZ);
 
 	bq_log("bq fuel gauge probe successfully, %s\n",
 			device2str[bq->chip]);
@@ -1152,7 +1023,6 @@ static inline bool is_device_suspended(struct bq_fg_chip *bq)
 {
 	return !bq->resume_completed;
 }
-
 
 static int bq_fg_suspend(struct device *dev)
 {
@@ -1179,7 +1049,6 @@ static int bq_fg_suspend_noirq(struct device *dev)
 
 }
 
-
 static int bq_fg_resume(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
@@ -1199,8 +1068,6 @@ static int bq_fg_resume(struct device *dev)
 	power_supply_changed(bq->fg_psy);
 
 	return 0;
-
-
 }
 
 static void bq_fg_remove(struct i2c_client *client)
@@ -1243,5 +1110,3 @@ module_i2c_driver(bq_fg_driver);
 MODULE_DESCRIPTION("TI BQ40Z50 Driver");
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Texas Instruments");
-
-
